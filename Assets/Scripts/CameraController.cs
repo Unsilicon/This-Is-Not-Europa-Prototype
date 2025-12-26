4a4c34c835c1ebfd2808be33557825c79a79fb2b
Unsilicon <Pei-HongYu@outlook.com>
2025-12-26 12:37:55 +0800

    feat: 添加相机视角跟随玩家功能

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player;
+
+    public float zOffset;
+
+    private void LateUpdate()
+    {
+        FollowPlayer();
+    }
+
+    // 让相机视角跟随玩家探测器
+    private void FollowPlayer()
+    {
+        transform.position = player.position + Vector3.forward * zOffset;
+    }
+}
